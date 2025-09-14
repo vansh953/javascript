4005ba3cd2dad751510ddad2ce1fa7f76e4a0ed5
vansh953 <vansh24164035@akgec.ac.in>
2025-09-14 12:49:15 +0530

    vansh

---
+++ datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all code as new version of js
+//alert(3+3) we are using nodejs not browser
+console.log(3+3);
+console.log("vansh");
+// ecma writes the standards of js
+let name = "vansh"
+let age = 18
+let login =false
+
+/* 
+primitive data types
+number = 2(power)53
+big int 
+string ""
+bollean true/false
+null standalone value (empty)
+undefined variable declared but value not assign
+symbol (uniqueness)
+*/
+
+console.log(typeof "vansh")
+console.log(typeof null)// object
+console.log(typeof undefined)//undefined
+
--- start.js
+++ start.js
@@ -1,1 +1,9 @@
 console.log("vansh");
+const accoountId =1444
+let email = "vansh24164035@akgec.ac.in"
+var accountPass =12345
+/* var not preferd */
+accountCity = "Bikaner"
+accountState = "Rajasthan"
+accountCountry = "India"
+console.table([accoountId,accountCity,accountState,accountCountry])
